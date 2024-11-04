7328a3deb47ab7490e688ba3a13c72cd2584dde4
Callum Kloos <Callumkloos@gmail.com>
2024-11-04 16:05:10 +0000

    more setup - theme, font, component removal

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,51 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: [
-					"Bricolage Grotesque Variable",
-					"Inter Variable",
-					"Inter",
-					...defaultTheme.fontFamily.sans,
-				],
+				sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				background: {
+					DEFAULT: "hsl(var(--background-1))",
+					"2": "hsl(var(--background-2))",
+					"3": "hsl(var(--background-3))",
+					menu: "hsl(var(--background-menu))",
+					hover: "hsl(var(--background-hover))",
+				},
+				foreground: {
+					DEFAULT: "hsl(var(--foreground))",
+					muted: "hsl(var(--foreground-muted))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+					accent: "hsl(var(--destructive-accent))",
+				},
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				success: {
+					DEFAULT: "hsl(var(--success))",
+					foreground: "hsl(var(--success-foreground))",
+				},
+				alt: {
+					DEFAULT: "hsl(var(--alt))",
+					foreground: "hsl(var(--alt-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				ring: "hsl(var(--ring))",
+				input: "hsl(var(--input))",
 			},
 		},
 	},
